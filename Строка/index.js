4b62c9e73847aa19e2443a7a8541d8aac97cfc68
Crocodility <47370976+Crocodility@users.noreply.github.com>
2020-02-06 17:03:12 +0200

    Add

---
+++ Строка/index.js
@@ -0,0 +1,2 @@
+let enter = prompt("Введи что угодно", 'Access denied')
+alert ('Длина в символах составляет ' + enter.length + ' символов.') 
---
+++ Функция/main.js
@@ -0,0 +1,16 @@
+function Hello(a,b){
+    if (a>b) {
+        return (a+1)
+    } else {
+        return (b-1)
+    }
+  
+} // Function Declaration
+
+let Bye = function(c,d){
+    if (c<d) {
+        return (c+2)
+    } else {
+        return (d-2)
+    }
+}
